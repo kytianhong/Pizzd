c7c7fd477900896eca8ae89d507bf1921044e7a6
s2142395 <s2142395@ed.ac.uk>
2023-10-02 23:03:53 +0100

    finish

--- src/main/java/uk/ac/ed/inf/interfaces/OrderValidator.java
+++ src/main/java/uk/ac/ed/inf/interfaces/OrderValidator.java
@@ -1,16 +1,15 @@
 package uk.ac.ed.inf.interfaces;
 
 import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
-import uk.ac.ed.inf.ilp.data.CreditCardInformation;
 import uk.ac.ed.inf.ilp.data.Order;
 import uk.ac.ed.inf.ilp.data.Pizza;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 
 import uk.ac.ed.inf.ilp.interfaces.OrderValidation;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.Scanner;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -40,17 +39,11 @@ public class OrderValidator implements OrderValidation{
      */
     public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants){
 
-        if (orderToValidate == null) {
-            orderToValidate.setOrderValidationCode(OrderValidationCode.TOTAL_INCORRECT);
-            return orderToValidate;
-        }
-
         String number = orderToValidate.getCreditCardInformation().getCreditCardNumber();
         String cvv = orderToValidate.getCreditCardInformation().getCvv();
-
         String exp_day = orderToValidate.getCreditCardInformation().getCreditCardExpiry();
-        LocalDate date = orderToValidate.getOrderDate();
-
+        LocalDate orderDate = orderToValidate.getOrderDate();
+        int totalCost = orderToValidate.getPriceTotalInPence();
         Pizza[] pizzas = orderToValidate.getPizzasInOrder();
 
         if ( number.length()!=16 ){
@@ -59,7 +52,7 @@ public class OrderValidator implements OrderValidation{
         } else if ( cvv.length() != 3) {
             //check CVV
             orderToValidate.setOrderValidationCode( OrderValidationCode.CVV_INVALID);
-        } else if (CardDateLegal( exp_day, date) ) {
+        } else if (CardDateLegal( exp_day, orderDate) ) {
             //check card expiration date
             orderToValidate.setOrderValidationCode( OrderValidationCode.EXPIRY_DATE_INVALID);
         } else if ( pizzas.length >4 ) {
@@ -68,20 +61,55 @@ public class OrderValidator implements OrderValidation{
         } else if ( pizzas.length == 0) {
             //if there is no pizza
             orderToValidate.setOrderValidationCode(OrderValidationCode.PIZZA_NOT_DEFINED);
-        }else {
+        }else if ( cost(pizzas) != totalCost) {
+            //if the order cost is valid
+            orderToValidate.setOrderValidationCode(OrderValidationCode.TOTAL_INCORRECT);
+        } else if (!pizzasFromMenu(pizzas, definedRestaurants)) {
+            //check if the menu items selected legal in the order
+            orderToValidate.setOrderValidationCode(OrderValidationCode.PIZZA_FROM_MULTIPLE_RESTAURANTS);
+        } else if (!restaurantOpen(Objects.requireNonNull(getRestaurant(pizzas, definedRestaurants)),orderDate)) {
+            //if the order is valid on the given date for the involved restaurants (opening days)
+            orderToValidate.setOrderValidationCode(OrderValidationCode.RESTAURANT_CLOSED);
+        } else {
             orderToValidate.setOrderValidationCode(OrderValidationCode.NO_ERROR);
         }
-            //check if the menu items selected legal in the order
-            //orderToValidate.setOrderValidationCode(OrderValidationCode.PIZZA_FROM_MULTIPLE_RESTAURANTS);
-
-            //if the order is valid on the given date for the involved restaurants (opening days)
-            //orderToValidate.setOrderValidationCode(OrderValidationCode.RESTAURANT_CLOSED);
 
         return orderToValidate;
     }
 
-    private boolean PizzaFrom(Pizza[] pizzas, Restaurant[] definedRestaurants) {
+    private Restaurant getRestaurant(Pizza[] pizzas, Restaurant[] definedRestaurants){
+        Set<Pizza> pizzaSet = new HashSet<>(Arrays.asList(pizzas));
+        Restaurant bestRestaurant = null;
+        for (Restaurant r : definedRestaurants) {
+            Pizza[] menu = r.menu();
+            //use set here, quicker
+            Set<Pizza> menuSet = new HashSet<>(Arrays.asList(menu));
+            if (menuSet.containsAll(pizzaSet)) {
+                bestRestaurant = r; // all pizza in one menu
+            }
+        }
+        return bestRestaurant; // not legal
+    }
+    private boolean restaurantOpen(Restaurant orderRestaurant, LocalDate orderDate ){
+        DayOfWeek dates = orderDate.getDayOfWeek();
+        DayOfWeek[] opendates = orderRestaurant.openingDays();
+        for (DayOfWeek d:opendates) {
+            if (d.equals(dates))return true;
+        }
         return false;
+    }
+    //check whether pizzas from one restaurant
+    private boolean pizzasFromMenu(Pizza[] pizzas, Restaurant[] definedRestaurants) {
+        Set<Pizza> pizzaSet = new HashSet<>(Arrays.asList(pizzas));
+        for (Restaurant r : definedRestaurants) {
+            Pizza[] menu = r.menu();
+            //use set here, quicker
+            Set<Pizza> menuSet = new HashSet<>(Arrays.asList(menu));
+            if (menuSet.containsAll(pizzaSet)) {
+                return true; // all pizza in one menu
+            }
+        }
+        return false; // pizzas in multi restaurants
     }
     private boolean CardDateLegal(String exp_day,LocalDate date){
         if (exp_day == null || exp_day.isEmpty() ){
@@ -90,14 +118,20 @@ public class OrderValidator implements OrderValidation{
             String[] parts = exp_day.split("/");
             int month = Integer.parseInt(parts[0]);
             int year = Integer.parseInt(parts[1]);
-
             if (year >= date.getYear()) {
-                if (month >= date.getMonthValue()) return true;
-                else return false;
+                return month >= date.getMonthValue();
             } else {
                 return false;
             }
         }
+    }
+    private int cost(Pizza[] pizzas){
+        int total=0;
+        for (Pizza p :pizzas) {
+            total += p.priceInPence();
+        }
+        total += 100;
+        return total;
     }
 
 }

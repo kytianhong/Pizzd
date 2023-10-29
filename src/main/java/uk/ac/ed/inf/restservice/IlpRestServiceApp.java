fe0bd07ffde494c59150aa11ea0d11bad8a724f2
s2142395 <s2142395@ed.ac.uk>
2023-10-29 15:08:59 +0000

    restservice

---
+++ src/main/java/uk/ac/ed/inf/restservice/IlpRestServiceApp.java
@@ -0,0 +1,11 @@
+package uk.ac.ed.inf.restservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlpRestServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(IlpRestServiceApp.class, args);
+    }
+
+}

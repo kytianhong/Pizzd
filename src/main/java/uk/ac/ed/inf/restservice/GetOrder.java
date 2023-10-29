ed230608bc1404ed590f97f7e94d1c12696c092b
s2142395 <s2142395@ed.ac.uk>
2023-10-29 18:30:07 +0000

    dated orders

--- src/main/java/uk/ac/ed/inf/restservice/GetOrder.java
+++ src/main/java/uk/ac/ed/inf/restservice/GetOrder.java
@@ -3,30 +3,35 @@ package uk.ac.ed.inf.restservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import uk.ac.ed.inf.restservice.data.Order;
-import uk.ac.ed.inf.restservice.data.Restaurant;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GetOrder {
     public static final String ORDER_URL = "orders";
-
-    public static void main(String[] args) {
+    public static Order[] orders;
+    public static void getOrders(String[] args) {
 //        if (args.length < 1){
 //            System.err.println("the base URL must be provided");
 //            System.exit(1);
 //        }
         var baseUrl = "https://ilp-rest.azurewebsites.net";
-//        var baseUrl = args[0];
-        if (baseUrl.endsWith("/") == false){
-            baseUrl += "/";
-        }
-
-        try {
-            var temp = new URL(baseUrl);
-        } catch (Exception x) {
-            System.err.println("The URL is invalid: " + x);
-            System.exit(2);
-        }
+        LocalDate date = LocalDate.of(2023, 9, 1);
+//        var baseUrl = args[1];
+//        var date = args[0];
+//        if (baseUrl.endsWith("/") == false){
+//            baseUrl += "/";
+//        }
+//
+//        try {
+//            var temp = new URL(baseUrl);
+//        } catch (Exception x) {
+//            System.err.println("The URL is invalid: " + x);
+//            System.exit(2);
+//        }
 
 
         ObjectMapper mapper = new ObjectMapper();
@@ -34,10 +39,20 @@ public class GetOrder {
         try {
             var orders = mapper.readValue(new URL(baseUrl + ORDER_URL), Order[].class);
             System.out.println("read all orders");
-//            System.out.println(orders[13].priceTotalInPence());
+            System.out.println(orders[13].priceTotalInPence());
+//            return orders;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        List<Order> extractedOrders = new ArrayList<>();
+        for (Order i:orders) {
+            if(i.orderDate() == date){
+                extractedOrders.add(i);
+            }
+        }
+//        Order[] datedOrders = extractedOrders.toArray();
+//        return datedOrders;
     }
 
 }
